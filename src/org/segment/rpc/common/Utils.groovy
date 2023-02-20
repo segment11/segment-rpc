77654182710af4fa2da5855648b07943405f3b5f
dingyong87 <dingyong87@163.com>
2023-02-20 15:21:34 +0800

    1. remove unused codes.

--- src/org/segment/rpc/common/Utils.groovy
+++ src/org/segment/rpc/common/Utils.groovy
@@ -1,22 +1,9 @@
 package org.segment.rpc.common
 
-
 import groovy.transform.CompileStatic
 
 @CompileStatic
 class Utils {
-    static boolean isWindows() {
-        System.getProperty('os.name').toLowerCase().contains('windows')
-    }
-
-    static String projectPath(String relativePath = '') {
-        def workDir = new File('.').absolutePath.replaceAll("\\\\", '/').
-                replace('/src/', '')
-        if (workDir[-1] == '.') {
-            return workDir[0..-2] + relativePath
-        }
-        workDir + relativePath
-    }
 
     static String localIp() {
         InetAddress.localHost.hostAddress
