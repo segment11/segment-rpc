eecd75bc23451dd60f5cca1cefa3c80807e42a69
dingyong87 <dingyong87@163.com>
2023-02-20 15:14:20 +0800

    1. change proxy using byte buddy.

--- src/org/segment/rpc/invoke/ProxyCreator.groovy
+++ src/org/segment/rpc/invoke/ProxyCreator.groovy
@@ -1,13 +1,16 @@
 package org.segment.rpc.invoke
 
 import groovy.transform.CompileStatic
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.TupleConstructor
+import net.bytebuddy.ByteBuddy
+import net.bytebuddy.implementation.InvocationHandlerAdapter
+import net.bytebuddy.matcher.ElementMatchers
 import org.segment.rpc.client.RpcClient
 import org.segment.rpc.server.handler.Req
 
 import java.lang.reflect.InvocationHandler
 import java.lang.reflect.Method
-import java.lang.reflect.Proxy
-import java.util.concurrent.ConcurrentHashMap
 
 @CompileStatic
 class ProxyCreator {
@@ -18,77 +21,76 @@ class ProxyCreator {
     private String context
 
     // local cache
-    private static Map<Key, Object> map = new ConcurrentHashMap<>()
+    private static Map<Key, Object> map = new HashMap<>()
 
     ProxyCreator(RpcClient client, String context) {
         this.client = client
         this.context = context
     }
 
-    Object create(Class<?> interfaceClass) {
+    public synchronized <T> T create(Class<T> interfaceClass) {
+        if (!interfaceClass.isInterface()) {
+            throw new IllegalArgumentException('must be interface')
+        }
         def key = new Key(client.uuid, context, interfaceClass.name)
         def r = map.get(key)
         if (r != null) {
-            return r
+            return (T) r
         }
 
-        Class<?>[] classes = [interfaceClass]
-        def newOne = Proxy.newProxyInstance(
-                interfaceClass.getClassLoader(),
-                classes,
-                new InvocationHandler() {
-                    @Override
-                    Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                        if ('toString' == method.name) {
-                            return "ProxyRpcCaller_" + this.class.name
-                        }
+        def newOne = new ByteBuddy()
+                .subclass(interfaceClass)
+                .name(interfaceClass.name + '_ProxyRpcCaller')
+                .method((ElementMatchers.isDeclaredBy(interfaceClass) &
+                        ElementMatchers.not(ElementMatchers.isStatic())) &
+                        ElementMatchers.not(ElementMatchers.isDeclaredBy(Object.class)))
+                .intercept(InvocationHandlerAdapter.of(new MethodInvocationHandler(client, context)))
+                .make()
+                .load(interfaceClass.classLoader)
+                .getLoaded().newInstance()
 
-                        def meta = new MethodMeta()
-                        meta.clazz = method.declaringClass.name
-                        meta.method = method.name
-                        meta.paramTypes = method.parameterTypes
-                        meta.args = args
-
-                        def req = new Req(context + HANDLER_URI, meta)
-                        req.isMethodInvoke = true
-                        def resp = client.sendSync(req)
-                        if (!resp.ok()) {
-                            throw new RpcMethodInvokeException('invoke error - ' + resp.status + ':' + resp.message)
-                        }
-                        resp.body
-                    }
-                }
-        )
-        map.put(key, newOne)
+        def oldOne = map.putIfAbsent(key, newOne)
+        if (oldOne != null) {
+            return (T) oldOne
+        }
         newOne
     }
 
     @CompileStatic
+    private static class MethodInvocationHandler implements InvocationHandler {
+        private RpcClient client
+
+        private String context
+
+        MethodInvocationHandler(RpcClient client, String context) {
+            this.client = client
+            this.context = context
+        }
+
+        @Override
+        Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            def meta = new MethodMeta()
+            meta.clazz = method.declaringClass.name
+            meta.method = method.name
+            meta.paramTypes = method.parameterTypes
+            meta.args = args
+
+            def req = new Req(context + HANDLER_URI, meta)
+            req.isMethodInvoke = true
+            def resp = client.sendSync(req)
+            if (!resp.ok()) {
+                throw new RpcMethodInvokeException('invoke error - ' + resp.status + ':' + resp.message)
+            }
+            resp.body
+        }
+    }
+
+    @CompileStatic
+    @TupleConstructor
+    @EqualsAndHashCode
     private static class Key {
         String clientUuid
         String context
         String interfaceClassName
-
-        Key(String clientUuid, String context, String interfaceClassName) {
-            this.clientUuid = clientUuid
-            this.context = context
-            this.interfaceClassName = interfaceClassName
-        }
-
-        @Override
-        boolean equals(Object obj) {
-            if (obj == null || !(obj instanceof Key)) {
-                return false
-            }
-
-            def that = (Key) obj
-            this.clientUuid == that.clientUuid && this.context == that.context &&
-                    this.interfaceClassName == that.interfaceClassName
-        }
-
-        @Override
-        int hashCode() {
-            (clientUuid + ',' + context + ',' + interfaceClassName).hashCode()
-        }
     }
 }
--- src/org/segment/rpc/invoke/RpcMethodInvokeException.groovy
+++ src/org/segment/rpc/invoke/RpcMethodInvokeException.groovy
@@ -1,10 +1,9 @@
 package org.segment.rpc.invoke
 
 import groovy.transform.CompileStatic
+import groovy.transform.InheritConstructors
 
 @CompileStatic
+@InheritConstructors
 class RpcMethodInvokeException extends RuntimeException {
-    RpcMethodInvokeException(String s) {
-        super(s)
-    }
 }
